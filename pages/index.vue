e91996a774e036329ab4abbe6c64a8ddde854110
Greg <gms64@georgetown.edu>
2020-08-17 17:05:42 -0400

    particles js

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,32 @@
 <template>
-  <div class="home-container">
-    <div class="u-full-width">
-      <h1 class="site-title">
-          greg<span class="txt-color">/</span>data
-      </h1>
-      <h5 class="subtitle" id="type-it-text">
-        <!-- Fancy Typed Text -->
-      </h5>
-      <p>
-        infrequently updated musings of a confused developer / data scientist
-      </p>
-      <span class="sm-txt"><em>I wouldn't read too much into anything here...</em></span>
+  <div id="particles-js">
+    <div class="home-container" >
+      <div class="u-full-width">
+        <h1 class="site-title">
+            greg<span class="txt-color">/</span>data
+        </h1>
+        <h5 class="subtitle" id="type-it-text">
+          <!-- Fancy Typed Text -->
+        </h5>
+        <p>
+          infrequently updated musings of a confused developer / data scientist
+        </p>
+        <span class="sm-txt"><em>I wouldn't read too much into anything here...</em></span>
 
-      <div class="m20">
-        <ColorModePicker />
+        <div class="m20">
+          <ColorModePicker />
+        </div>
       </div>
+      <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+      <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     </div>
-    <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-  </div>
+</div>
 </template>
 
 <script>
 import TypeIt from 'typeit'
 import ColorModePicker from '@/components/ColorModePicker'
-
+ 
 
 export default {
   components: {
@@ -57,9 +60,22 @@ export default {
     console.log("hey you, stop it.")
     console.log("110 111 32 114 101 97 108 108 121 44 32 115 116 111 112 32 105 116 46")
 
+    particlesJS.load('particles-js', '/particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+
   }
 
   
 }
 </script>
 
+
+<style>
+#particles-js canvas {
+    position:absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+}
+</style>
