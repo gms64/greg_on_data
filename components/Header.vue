f12c089076b950c13895070b98c285fe41f6f4c1
Greg <gms64@georgetown.edu>
2020-11-17 23:03:53 -0500

    nav menu

--- components/Header.vue
+++ components/Header.vue
@@ -2,14 +2,12 @@
     <header class="header">
       <nuxt-link to="/" class="logo">g<span class="txt-color">/</span>d</nuxt-link>
       <input class="menu-btn" type="checkbox" id="menu-btn" v-model="menuChecked"/>
-      <div class="row middle-xs reverse">
-          <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-          <li class="mobile-color-mode" @click="colors.push(colors.shift()); $colorMode.preference = colors[0]">
-            <a aria-label="Dark Mode">
-              <img class="icon-filter" style="color:#fff;" height="16" width="16" src="/icon/dark-mode.svg" alt="Dark Mode"/>
-            </a>
-          </li>        
-      </div>
+      <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+      <li class="mobile-color-mode" @click="colors.push(colors.shift()); $colorMode.preference = colors[0]">
+        <a aria-label="Dark Mode">
+          <img class="icon-filter" style="color:#fff;" height="16" width="16" src="/icon/dark-mode.svg" alt="Dark Mode"/>
+        </a>
+      </li>        
       
       <ul class="menu">
         <li><nuxt-link to="/blog/" @click.native="menuChecked=false">blog</nuxt-link></li>
